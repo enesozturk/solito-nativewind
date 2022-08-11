3736af9cc9291dc8a45748583d7ced617da8762b
Enes Ozturk <enesozturk.d@gmail.com>
2022-08-11 21:48:47 +0300

    chore: initialize project

---
+++ packages/app/features/home/screen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export function HomeScreen() {
+  return (
+    <View className="flex flex-row flex-1 items-center">
+      <Text>Home Screen</Text>
+    </View>
+  )
+}
